36bf9580c6f1305c111b36d9d02b58916a71e371
Abhishek Saksena <as4928@energon1.cims.nyu.edu>
2012-04-30 05:53:50 -0400

    Final Push

--- erlang/msg_space.erl
+++ erlang/msg_space.erl
@@ -17,8 +17,9 @@ runsimulation() ->
 
 	io:format(" - Client Spawning Initiated -  ~n~n", []),
 
+	{_, S, M} = now(),
 	%% Params: (0, Number of clients, 0, Messages per Client)
-	initiatespawn(0, 6, 0, 50, Messagedb),
+	initiatespawn(0, 6, 0, 10, Messagedb, S, M),
 	
 	% Simple message return test
 	%returnmessage("Bonda", "hi ", Messagedb).
@@ -31,11 +32,11 @@ runsimulation() ->
 %% 	  	    (so (0, 6) makes 6 clients)
 %% CharSt, CharFin: Message number start, finish 
 %% 	   	    (so (0, 10) sends 10 messages per client 
-initiatespawn(UsrFin, UsrFin, _CharSt, _CharFin, _Msgdb) -> done;
-initiatespawn(UsrSt, UsrFin, CharSt, CharFin, Msgdb) -> 
-	{_, S, M} = now(),
+
+initiatespawn(UsrFin, UsrFin, _CharSt, _CharFin, _Msgdb, _S, _M) -> done;
+initiatespawn(UsrSt, UsrFin, CharSt, CharFin, Msgdb, S, M) -> 
 	spawn(msg_space, fireupdates, [[66,111,110,100,UsrSt], CharSt, CharFin, Msgdb,S,M] ),
-	initiatespawn(UsrSt+1, UsrFin, CharSt, CharFin, Msgdb). 
+	initiatespawn(UsrSt+1, UsrFin, CharSt, CharFin, Msgdb, S, M). 
 
 
 %% Send out updates, and removals
@@ -45,12 +46,12 @@ fireupdates(User, Count, End, Messagedb, S, M) ->
 	Messagedb ! {post, {User, [104, 105, Count]}},
 	fireupdates(User, Count+1, End, Messagedb, S, M).
 
-undoupdates(User, End,   End, Messagedb, S, M) -> 
-	%{_, Ss, Mm} = now(),
-	%io:format("~w last post: ~ws, ~wms~n", [User,Ss-S,Mm-M]),
-	%% Careful of next line
-	Messagedb ! {status, User, S, M},
+undoupdates(User, End,   End, _Messagedb, S, M) -> 
+	{_, Ss, Mm} = now(),
+	io:format("~w  Completed: ~wms~n", [User, (Ss-S)*1000000+(Mm-M)]),
+	%Messagedb ! {status, User, S, M},
 	done;
+
 undoupdates(User, Count, End, Messagedb, S, M) ->
 	Messagedb ! {remove, {User, [104, 105, Count]}},
 	undoupdates(User, Count+2, End, Messagedb, S, M).
